c65d32ecdcfd45da267476ee95dec9875d5b5797
GET Academy <terje@getacademy.no>
2022-03-10 15:25:54 +0100

    Update GameWindow.cs

--- CarRacingOO/View/GameWindow.cs
+++ CarRacingOO/View/GameWindow.cs
@@ -1,8 +1,33 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CarRacingOO.View
 {
     internal class GameWindow : Window
     {
+        private Canvas _canvas;
+
+        public GameWindow()
+        {
+            Width = 525;
+            Height = 517;
+            Title = "Car racing OO";
+            _canvas = new Canvas
+            {
+                Background = new SolidColorBrush(Colors.Gray)
+            };
+
+            _canvas.KeyDown += KeyDown;
+            _canvas.KeyUp += KeyUp;
+        }
+
+        private void KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+        }
+
+        private void KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+        }
     }
 }
